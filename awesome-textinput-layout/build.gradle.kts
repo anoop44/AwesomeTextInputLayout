a7f54a5b13bc0d2f682c98202d8285cb543cb1e2
Anoop <anoopvvs@gmail.com>
2020-02-11 09:37:41 +0530

    Added bintray publishing

--- awesome-textinput-layout/build.gradle.kts
+++ awesome-textinput-layout/build.gradle.kts
@@ -1,13 +1,16 @@
+import com.novoda.gradle.release.PublishExtension
+
 plugins {
     id("com.android.library")
     kotlin("android")
+    id("com.novoda.bintray-release")
 }
 
 android {
     compileSdkVersion(29)
     defaultConfig {
         minSdkVersion(14)
-        versionName = "1.0"
+        versionName = "1.0.0"
         versionCode = 1000
     }
 
@@ -19,4 +22,13 @@ android {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.61")
+}
+
+configure<PublishExtension> {
+    userOrg = "anoop44"
+    groupId = "ss.anoop"
+    artifactId = "awesome-textinput-layout"
+    publishVersion = "1.0.0"
+    desc = "An EditText decorator inspired by the EditText fields in Add New Contact screen of Google's Contact App"
+    website = "https://github.com/anoop44/AwesomeTextInputLayout"
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:4.0.0-alpha09")
+        classpath ("com.android.tools.build:gradle:3.5.3")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
-
+        classpath ("com.novoda:bintray-release:0.9.2")
     }
 }
 
